21c3b741d9e40fe1e7fa15a5f02cc2b4018b6bc9
冯超凡 <2198604500@qq.com>
2026-06-19 11:36:16 +0800

    使用种子向数据库批量添加数据

---
+++ seeders/20260619031643-article.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    const articles = [];
+    const count = 100;
+
+    for(let i =0; i < count ; i++) {
+      const article = {
+        title:  `文章的标题 ${i + 1}`,
+        content : `文章的内容 ${i + 1}`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      articles.push(article)
+    }
+    await queryInterface.bulkInsert('Articles', articles, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Articles', null, {})
+  }
+};
