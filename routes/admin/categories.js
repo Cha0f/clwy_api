abcaaec16f8f280ecf13435fe25b31194af316ca
冯超凡 <2198604500@qq.com>
2026-06-21 00:00:00 +0800

    分类表关联了课程表，处理删除分类时避免孤儿记录

--- routes/admin/categories.js
+++ routes/admin/categories.js
@@ -94,6 +94,12 @@ router.delete('/:id', async function (req, res) {
   try {
     // 查询分类
     const category = await getCategory(req);
+    // 查询对应课程数量
+    const count = await Course.count({ where: { categoryId: req.params.id } });
+    // 判断数量
+    if (count > 0) {
+      throw new Error('当前分类有课程，无法删除。');
+    }
     // 删除分类
     await category.destroy();
     // 返回删除分类的结果
@@ -140,12 +146,8 @@ function filterBody(req) {
 async function getCategory(req) {
   // 获取分类id
   const { id } = req.params;
-  // 定义查询条件
-  const condition = {
-    include: [{ model: Course, as: 'course' }],
-  };
   // 查询当前分类
-  const categories = await Category.findByPk(id, condition);
+  const categories = await Category.findByPk(id);
   // 如果没有找到
   if (!categories) {
     throw new NotFondError(`ID: ${id}的分类没有找到。`);
